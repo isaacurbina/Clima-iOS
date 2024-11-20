eea99282b0ebf72bb11b6587123aee55739e97b3
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-19 20:11:07 -0600

    144. Learn to use the UITextField

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
+	// MARK: - Outlets
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    
+	@IBOutlet weak var searchTextField: UITextField!
+	
+	// MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+		searchTextField.delegate = self
     }
 
-
+	// MARK: - Actions
+	@IBAction func searchPressed(_ sender: UIButton) {
+		performSearch()
+	}
+	
+	// MARK: UITextFieldDelegate
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		performSearch()
+		return true
+	}
+	
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		// TODO(use searchTextField.text to get the weather for that city.)
+		searchTextField.text = ""
+	}
+	
+	func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+		if textField.text != "" {
+			return true
+		} else {
+			textField.placeholder = "Type something..."
+			return false
+		}
+	}
+	
+	// MARK: private functions
+	private final func performSearch() {
+		searchTextField.endEditing(true)
+		print(searchTextField.text!)
+	}
 }
 
 
