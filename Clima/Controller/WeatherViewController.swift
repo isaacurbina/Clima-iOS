04a6e74abd087d3b268eee3f06baa6a4a18dd6a0
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-21 17:31:54 -0600

    156. Updating the UI by Using the DispatchQueue

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -52,7 +52,10 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
 	
 	// MARK: WeatherManagerDelegate
 	func didUpdateWeather(_ weather: WeatherModel) {
-		print("didUpdateWeather(\(weather))")
+		DispatchQueue.main.async {
+			self.temperatureLabel.text = weather.temperatureString
+			self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+		}
 	}
 	
 	func didFailWithError(_ error: Error) {
